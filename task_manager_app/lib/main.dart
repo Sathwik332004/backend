d207ef802187dfb01fa67ec3024dee52a2f96910
sathwik <sathwikn67@gmail.com>
2025-12-27 22:52:00 +0530

    Initialize Flutter mobile application structure

---
+++ task_manager_app/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Basics App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello Flutter 👋',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
